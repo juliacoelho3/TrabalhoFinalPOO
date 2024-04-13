70ab42564e35f61e4dc7fe9174c11bfc7d7546fc
igor-andrade7 <cadastros.igorandrade@gmail.com>
2024-04-13 17:15:20 -0300

    Criados os atributos, métodos e construtores das classes Product, Order e OrderItem

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -1,5 +1,84 @@
 package entities;
 
-public class Order {
+import java.time.Instant;
+import java.util.Objects;
 
+public class Order {
+	
+	private Integer id;
+	private Instant issueDate;
+	private Instant deliveryDate;
+	private Double totalValue;
+	private String observation;
+
+	public Order() {
+	}
+
+	public Order(Integer id, Instant issueDate, Instant deliveryDate, Double totalValue, String observation) {
+		super();
+		this.id = id;
+		this.issueDate = issueDate;
+		this.deliveryDate = deliveryDate;
+		this.totalValue = totalValue;
+		this.observation = observation;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Instant getIssueDate() {
+		return issueDate;
+	}
+
+	public void setIssueDate(Instant issueDate) {
+		this.issueDate = issueDate;
+	}
+
+	public Instant getDeliveryDate() {
+		return deliveryDate;
+	}
+
+	public void setDeliveryDate(Instant deliveryDate) {
+		this.deliveryDate = deliveryDate;
+	}
+
+	public Double getTotalValue() {
+		return totalValue;
+	}
+
+	public void setTotalValue(Double totalValue) {
+		this.totalValue = totalValue;
+	}
+
+	public String getObservation() {
+		return observation;
+	}
+
+	public void setObservation(String observation) {
+		this.observation = observation;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Order other = (Order) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
 }
--- src/entities/OrderItem.java
+++ src/entities/OrderItem.java
@@ -1,5 +1,71 @@
 package entities;
 
+import java.util.Objects;
+
 public class OrderItem {
 
+	private Integer id;
+	private Double unitValue;
+	private Integer quantity;
+	private Double descountValue;
+	
+	public OrderItem() {
+	}
+
+	public OrderItem(Integer id, Double unitValue, Integer quantity, Double descountValue) {
+		super();
+		this.id = id;
+		this.unitValue = unitValue;
+		this.quantity = quantity;
+		this.descountValue = descountValue;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Double getUnitValue() {
+		return unitValue;
+	}
+
+	public void setUnitValue(Double unitValue) {
+		this.unitValue = unitValue;
+	}
+
+	public Integer getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(Integer quantity) {
+		this.quantity = quantity;
+	}
+
+	public Double getDescountValue() {
+		return descountValue;
+	}
+
+	public void setDescountValue(Double descountValue) {
+		this.descountValue = descountValue;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		OrderItem other = (OrderItem) obj;
+		return Objects.equals(id, other.id);
+	}
 }
