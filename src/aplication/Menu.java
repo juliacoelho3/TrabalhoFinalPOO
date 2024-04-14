d80f31066e691a9b14a9d71cce821b63f30b7c4c
igor-andrade7 <cadastros.igorandrade@gmail.com>
2024-04-13 21:26:43 -0300

    Criada classe Menu que será usada para auxiliar o programa principal na formatação da interface do aplicativo

---
+++ src/aplication/Menu.java
@@ -0,0 +1,12 @@
+package aplication;
+
+public class Menu {
+	
+	public static String menuInicial() {
+		return "###############################"
+				+ "\n      MENU INICIAL"
+				+ "\n   PRESSIONE UMA OPÇÃO:"
+				+ "\n###############################";
+	}
+
+}
